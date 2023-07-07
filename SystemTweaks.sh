214623b5eb5651d47d61832fbe803aa6ecfa1cc8
Niko <nikitafander@gmail.com>
2023-07-07 20:06:14 +0400

    modified:   SystemTweaks.sh

--- SystemTweaks.sh
+++ SystemTweaks.sh
@@ -9,8 +9,8 @@ fi
 
 echo "Applying system tweaks"
 
-sudo echo 'vm.max_map_count=1048576' >>/etc/sysctl.conf
-sudo echo 'PULSE_LATENCY_MSEC=60' >>/etc/profile.d/pulselatency.sh
-sudo echo 'RADV_PERFTEST=gpl' >>/etc/profile.d/gpl.sh
+echo 'vm.max_map_count=1048576' >>/etc/sysctl.conf
+echo 'PULSE_LATENCY_MSEC=60' >>/etc/profile.d/pulselatency.sh
+echo 'RADV_PERFTEST=gpl' >>/etc/profile.d/gpl.sh
 
 echo "Done!"
