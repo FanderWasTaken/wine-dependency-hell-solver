57c3d24d2092dae8c4a4ad74f660e66a2b687514
FanderWasTaken <nikitafander@gmail.com>
2024-12-20 11:09:58 +0400

    Download links update - Reverted back to using regular links for downloads, avoiding Intertet Archive as it seems to provide extremely low download speeds - Removed download of vcrun6sp for now

--- WDHS.sh
+++ WDHS.sh
@@ -29,11 +29,11 @@ wait
 
 urls=(
     'https://7-zip.org/a/7z2409-x64.exe'
-    'https://web.archive.org/web/20241210090520/https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe'
-    'https://web.archive.org/web/20241122130514/http://us.download.nvidia.com/Windows/9.09.0428/PhysX_9.09.0428_SystemSoftware.exe'
+    'https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe'
+    'http://us.download.nvidia.com/Windows/9.09.0428/PhysX_9.09.0428_SystemSoftware.exe'
     'https://github.com/abbodi1406/vcredist/releases/download/v0.85.0/VisualCppRedist_AIO_x86_x64.exe'
-    'https://web.archive.org/web/20241001233748/https://download.microsoft.com/download/vc60pro/Update/2/W9XNT4/EN-US/VC6RedistSetup_deu.exe'
-    'https://web.archive.org/web/20210126114402/http://download.microsoft.com/download/1/9/f/19fe4660-5792-4683-99e0-8d48c22eed74/Vs6sp6.exe'
+    'https://download.microsoft.com/download/vc60pro/Update/2/W9XNT4/EN-US/VC6RedistSetup_deu.exe'
+    #    'https://web.archive.org/web/20210126114402/http://download.microsoft.com/download/1/9/f/19fe4660-5792-4683-99e0-8d48c22eed74/Vs6sp6.exe'
     'https://dl.winehq.org/wine/wine-mono/9.4.0/wine-mono-9.4.0-x86.msi'
     'https://github.com/Nevcairiel/LAVFilters/releases/download/0.79.2/LAVFilters-0.79.2-Installer.exe'
     'https://www.openal.org/downloads/oalinst.zip'
@@ -44,7 +44,7 @@ outs=(
     'physx/PhysX.exe'
     'vcredist/visual.exe'
     'vcredist/vcrun6.exe'
-    'vcredist/vcrun6sp6.exe'
+    #    'vcredist/vcrun6sp6.exe'
     'mono/wine-mono.msi'
     'codecs/lav.exe'
     'codecs/oal.zip'
@@ -59,7 +59,7 @@ echo " ~ Extracting files..."
 
 7z x -oinstallers/directx/ installers/directx/directx.exe -y >installers/null
 7z x -oinstallers/vcredist/vcrun6/ installers/vcredist/vcrun6.exe -y >installers/null
-7z x -oinstallers/vcredist/vcrun6sp6/ installers/vcredist/vcrun6sp6.exe -y >installers/null
+# 7z x -oinstallers/vcredist/vcrun6sp6/ installers/vcredist/vcrun6sp6.exe -y >installers/null
 7z x -oinstallers/codecs/ installers/codecs/oal.zip -y >installers/null
 
 echo " ~ Cleaning up..."
@@ -68,7 +68,7 @@ torm=(
     'null'
     'directx/directx.exe'
     'vcredist/vcrun6.exe'
-    'vcredist/vcrun6sp6.exe'
+    #    'vcredist/vcrun6sp6.exe'
     'codecs/oal.zip'
 )
 
